2caa0c8204a6545f58a14fbcf7bc602eff1b0b6c
MaksymLuszczaniec <luszczaniec24@outlook.com>
2024-10-17 21:58:07 +0200

    Add Angular form validation
    Ensure area input is positive and required. Added error messages for validation feedback.

--- ClientApp/src/app/quote-form/quote-form.component.ts
+++ ClientApp/src/app/quote-form/quote-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,24 +11,28 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent {
+  
   city: string = 'Stockholm';
-  area: number = 0;
+  area: number | null = null;  // setting the field to null at the beginning of validation
   windowCleaning: boolean = false;
   balconyCleaning: boolean = false;
   trashRemoval: boolean = false;
 
   constructor(private router: Router) {}
 
-  onSubmit() {
-    this.router.navigate(['/results'], {
-      queryParams: {
-        city: this.city,
-        area: this.area,
-        windowCleaning: this.windowCleaning.toString(),  // Przekazujemy jako tekst
-        balconyCleaning: this.balconyCleaning.toString(),
-        trashRemoval: this.trashRemoval.toString()
-      }
-    });
+  onSubmit(form: NgForm) {
+    if (form.valid) {  // Checking whether the form is valid validation
+      this.router.navigate(['/results'], {
+        queryParams: {
+          city: this.city,
+          area: this.area,
+          windowCleaning: this.windowCleaning,
+          balconyCleaning: this.balconyCleaning,
+          trashRemoval: this.trashRemoval
+        }
+      });
+    }
   }
 }
+
 
--- obj/Debug/net8.0/CleanUpQuotes.AssemblyInfo.cs
+++ obj/Debug/net8.0/CleanUpQuotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanUpQuotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e183faa369b5e1a01f03f241e538b8ff71d35ba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a252a4d7669d142499d85d32bcb482ea6225de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanUpQuotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanUpQuotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
